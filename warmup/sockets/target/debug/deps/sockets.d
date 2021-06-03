8d07b10dfc2947feb84dc4274695d2b899ba0496
jkabc123 <yang.piyi@gmail.com>
2021-06-03 15:36:12 +1000

    Done

--- warmup/sockets/target/debug/deps/sockets.d
+++
@@ -1,5 +0,0 @@
-/Users/pyang/Desktop/ToDo/projects/github/ruraft/warmup/sockets/target/debug/deps/sockets: src/main.rs
-
-/Users/pyang/Desktop/ToDo/projects/github/ruraft/warmup/sockets/target/debug/deps/sockets.d: src/main.rs
-
-src/main.rs:
--- warmup/sockets/target/debug/sockets.d
+++
@@ -1,1 +0,0 @@
-/Users/pyang/Desktop/ToDo/projects/github/ruraft/warmup/sockets/target/debug/sockets: /Users/pyang/Desktop/ToDo/projects/github/ruraft/warmup/sockets/src/main.rs
