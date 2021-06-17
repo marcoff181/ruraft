8ad5958c79030ee02f84a766901ec1c8f7da85e0
Peter Young <yang.piyi@gmail.com>
2021-06-17 11:21:53 +1000

    Work in progress

--- raftmodel/src/raftserver.rs
+++ raftmodel/src/raftserver.rs
@@ -241,4 +241,38 @@ mod tests {
 
         assert_eq!(servers[1].log, servers[2].log);
     }
+
+    fn make_log(terms: Vec<usize>) -> Vec<LogEntry<String>> {
+        let mut result: Vec<LogEntry<String>> = vec![LogEntry::default()];
+        for x in terms {
+            result.push(LogEntry {
+                term: x,
+                item: "a".to_string(),
+            });
+        }
+        result
+    }
+
+    fn test_figure_6() {
+        let mut servers = vec![
+            RaftServer::new(vec![LogEntry::default()]),
+            RaftServer::new(make_log(vec![1, 1, 1, 2, 3, 3, 3, 3])),
+            RaftServer::new(make_log(vec![1, 1, 1, 2, 3])),
+            RaftServer::new(make_log(vec![1, 1, 1, 2, 3, 3, 3, 3])),
+            RaftServer::new(make_log(vec![1, 1])),
+            RaftServer::new(make_log(vec![1, 1, 1, 2, 3, 3, 3])),
+        ];
+
+        for server in &mut servers {
+            server.current_term = 3;
+        }
+
+        run_message(
+            RaftMessage::BecomeLeader {
+                dest: 1,
+                followers: (0..6).collect(),
+            },
+            &mut servers,
+        );
+    }
 }
