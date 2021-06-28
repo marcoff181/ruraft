5d184ca5a9265fd27c78fd54680af616bd572157
jkabc123 <yang.piyi@gmail.com>
2021-06-29 09:56:30 +1000

    Work in progress

--- raftmodel/src/lib.rs
+++ raftmodel/src/lib.rs
@@ -1,14 +1,36 @@
 //!
 //! # Raft Model
-//! `raftmodel` aims to provide pure rust implementations
-//! of the raft consensus algorithm.
+//! `raftmodel` aims to provide the rust implementations
+//! of logic model for the raft consensus algorithm.
 //!
 //! # The big picture
+//! `raftmodel` is a crate to provide a pure logic model for the raft algorithm.
+//! It aims to stay as close as possible to its formal TLA+ specification ([raft.tla](https://github.com/ongardie/raft.tla/blob/master/raft.tla)).
+//! # Quick Start
+//! ```
+//! use crate::raftmodel::*;
+//!
+//! fn make_log(terms: Vec<usize>) -> Vec<LogEntry<String>> {
+//!    let mut result: Vec<LogEntry<String>> = vec![LogEntry::default()];
+//!    for x in terms {
+//!        result.push(LogEntry {
+//!            term: x,
+//!            item: "a".to_string(),
+//!        });
+//!    }
+//!    result
+//!}
+//!
+//! let mut servers = vec![
+//!    RaftServer::new(vec![LogEntry::default()]),
+//!    RaftServer::new(make_log(vec![1, 1, 1, 2, 3, 3, 3, 3])),
+//!    RaftServer::new(make_log(vec![1, 1, 1, 2, 3])),
+//!    RaftServer::new(make_log(vec![1, 1, 1, 2, 3, 3, 3, 3])),
+//!    RaftServer::new(make_log(vec![1, 1])),
+//!    RaftServer::new(make_log(vec![1, 1, 1, 2, 3, 3, 3])),
+//! ];
+//! ```
 
-pub mod raftlog;
-pub mod raftmessage;
-pub mod raftserver;
+mod raftmodel;
 
-pub use crate::raftlog::*;
-pub use crate::raftmessage::*;
-pub use crate::raftserver::*;
+pub use crate::raftmodel::*;
---
+++ raftmodel/src/raftmodel/mod.rs
@@ -0,0 +1,7 @@
+mod raftlog;
+mod raftmessage;
+mod raftserver;
+
+pub use raftlog::*;
+pub use raftmessage::*;
+pub use raftserver::*;
